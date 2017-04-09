3abcd28be155d68f295d8707b56f31c72f657c7d
Pham Quang Thien <thienpq88@gmail.com>
2017-04-10 00:08:21 +0700

    12 camera doing

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	public float smoothing = 5.0f;
+
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = this.transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 camNewPos = target.position + offset;
+		this.transform.position = Vector3.Lerp (this.transform.position,
+			camNewPos, smoothing * Time.deltaTime);
+		
+	}
+}
